b81a59fd9fdbfa9c57a2416de7ad443a6efcd213
Praveen Kumar Konka <praveenk@hiof.no>
2021-06-27 18:46:35 +0200

    Add project files.

---
+++ ArvatoAPItest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArvatoAPItest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
