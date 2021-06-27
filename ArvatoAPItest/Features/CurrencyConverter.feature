1fd0be6d2a5d341b315ffa1d5c3b0907b4e51164
Praveen Kumar Konka <praveenk@hiof.no>
2021-06-27 19:30:26 +0200

    Api Testing

---
+++ ArvatoAPItest/Features/CurrencyConverter.feature
@@ -0,0 +1,34 @@
+﻿Feature: CurrencyConverter
+	
+
+@ConvertFromEURToNINR
+Scenario: Convertfrom EUR to INR
+	Given the amount is 50	
+	Given Convert from currency is EUR
+	Given to Currency is INR
+	When the amount is convertd
+	Then the result in INR is 4429.517
+
+@ConvertFromNOKToINR
+Scenario: Convertfrom EUR to NOK
+	Given the amount is 100	
+	Given Convert from currency is EUR
+	Given to Currency is NOK
+	When the amount is convertd
+	Then the result in NOK is 1012.516
+
+	@ConvertFromUSDToNOK
+Scenario: Convertfrom USD to NOK
+	Given the amount is 10	
+	Given Convert from currency is USD
+	Given to Currency is NOK
+	When the amount is convertd
+	Then the result should be 0
+
+	@ConvertFromEURToNOKwithNegitives
+Scenario: Convertfrom EUR to NOK with Negitive amount
+	Given the amount is -100	
+	Given Convert from currency is EUR
+	Given to Currency is NOK
+	When the amount is convertd
+	Then the result for NOK in negative amount is -1012.516
---
+++ ArvatoAPItest/Steps/CurrencyConverter.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowCalculator.Specs.Steps
+{
+    [Binding]
+    public sealed class CurrencyConverter
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        private readonly global::CurrencyConverter.CurrencyConverter _currencyConverter = new global::CurrencyConverter.CurrencyConverter();
+        private double _result;
+        private System.Exception exception;
+
+
+        public CurrencyConverter(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the amount is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _currencyConverter.Amount =  number;
+        }
+
+        [Given("Convert from currency is (.*)")]
+        public void GivenFromCurrencyIs(string fromCurrency)
+        {
+            _currencyConverter.FromCurrency = fromCurrency;
+        }
+
+        [Given("to Currency is (.*)")]
+        public void GivenTOCurrencyIs(string toCurrency)
+        {
+            _currencyConverter.ToCurrency = toCurrency;
+        }
+
+        [When("the amount is convertd")]
+        public void WhenTheAmountIsConverted()
+        {
+            try
+            {
+                _result = _currencyConverter.Convert();
+            }
+            catch (System.Exception ex)
+            {
+                _result = 0;
+                exception = ex;
+            }
+        }
+      
+        [Then("the result in NOK is (.*)")]
+        public void ThenTheResultShouldBe(double result)
+        {
+            _result.Should().Be(result);
+
+        }
+        [Then("the result in INR is (.*)")]
+        public void AfterEurTOInr(double result)
+        {
+            _result.Should().Be(result);
+
+        }
+        [Then("the result should be(.*)")]
+        public void Subscription(double result)
+        {
+            _result.Should().Be(result);
+
+        }
+        
+            [Then("the result for NOK in negative amount is(.*)")]
+        public void Negitiveamount(double result)
+        {
+            _result.Should().Be(result);
+
+        }
+    }
+}
